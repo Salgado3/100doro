d90b84623c03afb6b63343ae105f03c362642b7d
mbcodes <mbcodes@protonmail.com>
2025-02-17 21:49:01 -0800

    added .setKey() for permissions to work

--- functions/create-user/src/main.js
+++ functions/create-user/src/main.js
@@ -3,8 +3,8 @@ import { Client, Databases, Users } from 'node-appwrite';
 export default async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID);
-  // .setKey(process.env.APPWRITE_FUNCTION_API_KEY);
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(req.headers['x-appwrite-key'] ?? '');
 
   const databases = new Databases(client);
 
